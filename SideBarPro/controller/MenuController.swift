de2344cc042779a94004322dc9c730fde2b32f4d
BakytDzhumabaev <djumabaevb@gmail.com>
2021-10-22 00:19:40 +0600

    implementation delegates protocol funcs

--- SideBarPro/controller/MenuController.swift
+++ SideBarPro/controller/MenuController.swift
@@ -28,4 +28,14 @@ class MenuController: UIViewController {
     }
 }
 
-
+extension MenuController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 4
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+    
+    
+}
