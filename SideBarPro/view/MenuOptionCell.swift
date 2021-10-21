3d27bf780a12cde6870511bf1458b0f8cf876d87
BakytDzhumabaev <djumabaevb@gmail.com>
2021-10-22 00:23:32 +0600

    menu option cell

---
+++ SideBarPro/view/MenuOptionCell.swift
@@ -0,0 +1,26 @@
+//
+//  MenuOptionCell.swift
+//  SideBarPro
+//
+//  Created by Bakyt Dzhumabaev on 22/10/21.
+//
+
+import UIKit
+
+class MenuOptionCell: UITableViewCell {
+
+    //Mark: - Properties
+    
+    //Mark: - Init
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    //Mark: - Handlers
+
+}
