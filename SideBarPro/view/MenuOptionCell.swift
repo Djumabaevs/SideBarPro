64dfc31c0b26f977d6b7a125f871ee6684a2f47d
BakytDzhumabaev <djumabaevb@gmail.com>
2021-10-23 00:23:16 +0600

    add relative to icons anchor

--- SideBarPro/view/MenuOptionCell.swift
+++ SideBarPro/view/MenuOptionCell.swift
@@ -38,7 +38,10 @@ class MenuOptionCell: UITableViewCell {
         iconImageView.heightAnchor.constraint(equalToConstant: 24).isActive = true
         iconImageView.widthAnchor.constraint(equalToConstant: 24).isActive = true
         
-        
+        addSubview(descriptionLabel)
+        descriptionLabel.translatesAutoresizingMaskIntoConstraints = false
+        descriptionLabel.centerYAnchor.constraint(equalTo: centerYAnchor).isActive = true
+        descriptionLabel.leftAnchor.constraint(equalTo: iconImageView.rightAnchor, constant: 12).isActive = true
     }
     
     required init?(coder: NSCoder) {
