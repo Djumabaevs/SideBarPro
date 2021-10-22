197312e9df7d997acfec047d9718eec17acf6938
BakytDzhumabaev <djumabaevb@gmail.com>
2021-10-23 00:06:54 +0600

    table view constraints to anchor

--- SideBarPro/controller/MenuController.swift
+++ SideBarPro/controller/MenuController.swift
@@ -29,6 +29,13 @@ class MenuController: UIViewController {
         tableView.dataSource = self
         
         tableView.register(MenuOptionCell.self, forCellReuseIdentifier: reuseIdentifier)
+        
+        view.addSubview(tableView)
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
+        tableView.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
+        tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+        tableView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
     }
 }
 
