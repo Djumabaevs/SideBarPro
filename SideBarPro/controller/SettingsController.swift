d1adce36512aeb457ad864b6f46332f8fcf2dd4f
BakytDzhumabaev <djumabaevb@gmail.com>
2021-10-24 11:52:50 +0600

    navBarAppearance

--- SideBarPro/controller/SettingsController.swift
+++ SideBarPro/controller/SettingsController.swift
@@ -27,24 +27,7 @@ class SettingsController: UIViewController {
     //Mark: - Handlers
     
     func configureUI() {
-        
-        
-        let navBarAppearance = UINavigationBarAppearance()
-               navBarAppearance.configureWithOpaqueBackground()
-               navBarAppearance.largeTitleTextAttributes = [.foregroundColor: largeTitleColor]
-               navBarAppearance.titleTextAttributes = [.foregroundColor: largeTitleColor]
-               navBarAppearance.backgroundColor = backgoundColor
 
-               navigationController?.navigationBar.standardAppearance = navBarAppearance
-               navigationController?.navigationBar.compactAppearance = navBarAppearance
-               navigationController?.navigationBar.scrollEdgeAppearance = navBarAppearance
-
-               navigationController?.navigationBar.prefersLargeTitles = preferredLargeTitle
-               navigationController?.navigationBar.isTranslucent = false
-               navigationController?.navigationBar.tintColor = tintColor
-               navigationItem.title = title
-        
-        
         navigationController?.isNavigationBarHidden = false
         view.backgroundColor = .white
         navigationController?.navigationBar.barTintColor = .darkGray
@@ -59,3 +42,30 @@ class SettingsController: UIViewController {
         dismiss(animated: true, completion: nil)
     }
 }
+
+extension UIViewController {
+func configureNavigationBar(largeTitleColor: UIColor, backgoundColor: UIColor, tintColor: UIColor, title: String, preferredLargeTitle: Bool) {
+    if #available(iOS 13.0, *) {
+        let navBarAppearance = UINavigationBarAppearance()
+        navBarAppearance.configureWithOpaqueBackground()
+        navBarAppearance.largeTitleTextAttributes = [.foregroundColor: largeTitleColor]
+        navBarAppearance.titleTextAttributes = [.foregroundColor: largeTitleColor]
+        navBarAppearance.backgroundColor = backgoundColor
+
+        navigationController?.navigationBar.standardAppearance = navBarAppearance
+        navigationController?.navigationBar.compactAppearance = navBarAppearance
+        navigationController?.navigationBar.scrollEdgeAppearance = navBarAppearance
+
+        navigationController?.navigationBar.prefersLargeTitles = preferredLargeTitle
+        navigationController?.navigationBar.isTranslucent = false
+        navigationController?.navigationBar.tintColor = tintColor
+        navigationItem.title = title
+
+    } else {
+        // Fallback on earlier versions
+        navigationController?.navigationBar.barTintColor = backgoundColor
+        navigationController?.navigationBar.tintColor = tintColor
+        navigationController?.navigationBar.isTranslucent = false
+        navigationItem.title = title
+    }
+}}
