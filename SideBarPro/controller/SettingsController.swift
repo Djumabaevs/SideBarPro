9935b9b664c9a6dce47187ee17ed8d3d1feb9bfa
BakytDzhumabaev <djumabaevb@gmail.com>
2021-10-24 09:46:11 +0600

    settings controller

---
+++ SideBarPro/controller/SettingsController.swift
@@ -0,0 +1,21 @@
+//
+//  SettingsController.swift
+//  SideBarPro
+//
+//  Created by Bakyt Dzhumabaev on 24/10/21.
+//
+
+import UIKit
+
+class SettingsController: UIViewController {
+    
+    //Mark: - Properties
+    
+    //Mark: - Init
+    
+    override func viewDidLoad() {
+        
+    }
+    
+    //Mark: - Handlers
+}
